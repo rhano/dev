b8310213d0d941d5aa2531dbb29df07854848ca9
rhano <rhano@rhano.remote.csb>
2022-04-08 09:37:21 -0400

    comitting new message

--- src/main/java/com/redhat/rosademo/views/MainView.java
+++ src/main/java/com/redhat/rosademo/views/MainView.java
@@ -23,7 +23,7 @@ public class MainView extends VerticalLayout {
     addButton.addClickShortcut(Key.ENTER); 
 
     add( 
-      new H1("Red Hat ROSA Demo Todo "),
+      new H1("Red Hat ROSA Demo Todo"),
       todosList,
       new HorizontalLayout(
         taskField,
