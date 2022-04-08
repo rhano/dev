2d7910042b55950749c2c099f16e969c9294154b
rhano <rhano@rhano.remote.csb>
2022-04-08 09:18:30 -0400

    first commit

---
+++ src/main/java/com/redhat/rosademo/RosaSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.rosademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosaSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosaSpringBootDemoApplication.class, args);
+	}
+
+}
